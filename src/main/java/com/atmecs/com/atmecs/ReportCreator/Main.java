1257d774a47bc20d23d69533364cd3c8f00646f4
Chetan <chetan@ATMECSINDT-049.(none)>
2014-08-05 16:58:34 +0530

    initial commit comment

---
+++ src/main/java/com/atmecs/com/atmecs/ReportCreator/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author chetan
+ *
+ */
+package com.atmecs.com.atmecs.ReportCreator;
+
+import org.testng.annotations.Test;
+
+public class Main {
+	@Test
+	public void main() {
+		System.out.println("DemoProject");
+	}
+
+}
