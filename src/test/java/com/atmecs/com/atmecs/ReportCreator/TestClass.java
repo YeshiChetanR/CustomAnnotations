c3a57a35d741857e9d093fd17be58ff1e64ab6ce
Chetan <chetan@ATMECSINDT-049.(none)>
2014-08-05 17:47:11 +0530

    commit for testng.xml

---
+++ src/test/java/com/atmecs/com/atmecs/ReportCreator/TestClass.java
@@ -0,0 +1,33 @@
+/**
+ * @author chetan
+ *
+ */
+package com.atmecs.com.atmecs.ReportCreator;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void testmethod() {
+		System.out.println("DemoProject");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Inside test two");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Inside test two");
+	}
+	
+	@Test
+	public void test4()
+	{
+		System.out.println("Inside test two");
+	}
+}
