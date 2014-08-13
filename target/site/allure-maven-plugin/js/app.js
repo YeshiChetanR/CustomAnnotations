3208027641b61bd79b4ca7eac420e1c08c409852
Chetan <chetan@ATMECSINDT-049.(none)>
2014-08-13 18:18:40 +0530

    initial commit

---
+++ target/site/allure-maven-plugin/js/app.js
@@ -0,0 +1,71 @@
+/*global angular */
+angular.module('allure', ['ngAnimate', 'ui.bootstrap', 'localStorageModule', 'ui.router',
+        'allure.filters', 'allure.services', 'allure.directives', 'allure.controllers', 'allure.charts',
+        'allure.testcase', 'allure.xUnit.controllers', 'allure.table', 'allure.pane', 'allure.features'])
+    .config(function($tooltipProvider) {
+        $tooltipProvider.options({appendToBody:true})
+    })
+    .config(function($httpProvider) {
+        $httpProvider.defaults.cache = true;
+    })
+    .config(function ($stateProvider, $urlRouterProvider, testcaseProvider) {
+        'use strict';
+        function processResponse(response) {
+            return response.data;
+        }
+        function testcasesResolve($http) {
+            return $http.get('data/graph.json').then(processResponse);
+        }
+        $urlRouterProvider.otherwise("/home");
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "templates/home.html",
+                controller: 'HomeCtrl',
+                resolve: {
+                    testsuites: function($http) {
+                        return $http.get('data/xunit.json').then(processResponse);
+                    }
+                }
+            })
+            .state('home.testsuite', {
+                url: "/:testsuiteUid"
+            })
+            .state('home.testsuite.expanded', {
+                url: '/expanded'
+            })
+            .state('graph', {
+                url: '/graph',
+                templateUrl: "templates/graph.html",
+                controller: 'GraphCtrl',
+                resolve: {
+                    testcases: testcasesResolve
+                }
+            })
+            .state('timeline', {
+                url: '/timeline',
+                templateUrl: "templates/timeline.html",
+                controller: 'TimelineCtrl',
+                resolve: {
+                    testcases: testcasesResolve
+                }
+            })
+            .state('features', {
+                url: '/features',
+                templateUrl: "templates/features.html",
+                controller: 'FeaturesCtrl',
+                resolve: {
+                    features: function($http) {
+                        return $http.get('data/behavior.json').then(processResponse);
+                    }
+                }
+            })
+            .state('features.story', {
+                url: '/:storyUid'
+            })
+            .state('features.story.expanded', {
+                url: '/expanded'
+            });
+        testcaseProvider.attachStates('features.story');
+        testcaseProvider.attachStates('home.testsuite');
+    });
---
+++ target/site/allure-maven-plugin/js/features/FeaturesCtrl.js
@@ -0,0 +1,90 @@
+angular.module('allure.features', [])
+.controller('FeaturesCtrl', function($scope, $state, status, features) {
+    function calculatePercents(item) {
+        item.percents = status.all.map(function(status) {
+            status = status.toLowerCase();
+            return {
+                count: item.statistic[status],
+                value: item.statistic[status]/item.statistic.total*100 || 0,
+                status: status
+            }
+        });
+    }
+    function getChartData() {
+        var unstable = $scope.unstableStories.length,
+            total = $scope.allStories.length,
+            stable = total - unstable;
+        return [
+            {value: stable, part: stable/total, name: 'passed'},
+            {value: unstable, part: unstable/total, name: 'failed'}];
+    }
+    function findStory(storyUid) {
+        var story;
+        $scope.features.some(function(feature) {
+            return story = feature.stories.filter(function(story) {
+                return story.uid === storyUid;
+            })[0];
+        });
+        //noinspection JSUnusedAssignment
+        return story;
+    }
+    function setStory(storyUid) {
+        var story = findStory(storyUid);
+        $scope.story = story;
+        $scope.features.forEach(function(feature) {
+            if(feature.stories.indexOf(story) !== -1) {
+                feature.expanded = true;
+            }
+        });
+    }
+    $scope.isState = function(statename) {
+        return $state.is(statename);
+    };
+    $scope.setStory = function(story) {
+        if(story === $scope.story) {
+            $state.go('features');
+        } else {
+            $state.go('features.story', {storyUid: story.uid});
+        }
+    };
+    $scope.expandFeature = function(feature, expanded) {
+        if(!expanded && feature.stories.indexOf($scope.story) !== -1) {
+            $state.go('features')
+        }
+        feature.expanded = expanded;
+    };
+    $scope.features = features.features;
+    $scope.allStories = $scope.features.reduce(function(all, feature) {
+        return all.concat(feature.stories);
+    }, []);
+    $scope.features.forEach(calculatePercents);
+    $scope.allStories.forEach(calculatePercents);
+    $scope.unstableStories = $scope.allStories.filter(function(story) {
+        return story.statistic.passed !== story.statistic.total;
+    });
+    $scope.chartData = getChartData();
+    $scope.chartTooltip = '<div><b>{{value}} {{value == 1 ? "story" : "stories"}} ({{data.part * 100 | number:0}}%)</b></div>' +
+        '<div>{{data.name}}</div>';
+    $scope.summaryText = [
+        'Everything is very bad!',
+        'There are many errors!',
+        'Some tests are not passed',
+        'All done. Good job!'
+    ][Math.floor($scope.chartData[0].part*3)];
+    $scope.testcase = {};
+    $scope.$watch('testcase.uid', function(testcaseUid) {
+        if(testcaseUid) {
+            $state.go('features.story.testcase', {testcaseUid: testcaseUid})
+        }
+    });
+    $scope.$on('$stateChangeSuccess', function(event, state, params) {
+        delete $scope.story;
+        delete $scope.testcase.uid;
+        if(params.storyUid) {
+            setStory(params.storyUid);
+        }
+        if(params.testcaseUid) {
+            $scope.testcase.uid = params.testcaseUid;
+        }
+    });
+});
